abd24e3c8e7bebbc50a86c8051580649b7d3a0f5
zhangtreefish <zhangtreefish@users.noreply.github.com>
2017-04-23 22:36:52 -0500

    use mLab addon to prepare for deployment to heroku

--- mongomart.js
+++ mongomart.js
@@ -51,7 +51,7 @@ var ITEMS_PER_PAGE = 5;
 // Hardcoded USERID for use with the shopping cart portion
 var USERID = "558098a65133816958968d88";
 
-MongoClient.connect('mongodb://localhost:27017/mongomart', function (err, db) {
+MongoClient.connect(process.env.dburi, function (err, db) {
     "use strict";
 
     assert.equal(null, err);
